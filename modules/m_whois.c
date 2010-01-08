4445c3e72ab22d0ad69b1f4a6855529e8e1fe668
radicand <none@none>
2010-01-08 01:38:33 +0000

    fixed bug #166 (rpl_whoiscertfp wrong numeric) updated rpl_whoisregnick to look nicer
    --HG--
    extra : convert_revision : svn%3A0106ce42-a294-4a64-99d6-01c6ecc9e238/trunk%4052

--- modules/m_whois.c
+++ modules/m_whois.c
@@ -443,7 +443,7 @@ whois_person(struct Client *source_p, struct Client *target_p)
 		}
 		else
 		sendto_one(source_p, form_str(RPL_WHOISREGNICK),
-			   me.name, source_p->name, target_p->name, " ");
+			   me.name, source_p->name, target_p->name, "this nick");
 	}
 
 	if(IsSSL(target_p)) {
