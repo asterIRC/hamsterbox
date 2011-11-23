710637196798ce131b3922766a92bc8d829d6608
Adam <Adam@anope.org>
2011-11-23 00:48:07 -0500

    Fixed detecting OpenSSL cipher methods on Ubuntu 11

--- contrib/setup-win32.h
+++ contrib/setup-win32.h
@@ -77,22 +77,22 @@
 #define HAVE_ERRNO_H 1
 
 /* Define to 1 if you have the `EVP_bf_cfb' function. */
-/* #undef HAVE_EVP_BF_CFB */
+/* #undef HAVE_EVP_BF_CFB64 */
 
 /* Define to 1 if you have the `EVP_cast5_cfb' function. */
-/* #undef HAVE_EVP_CAST5_CFB */
+/* #undef HAVE_EVP_CAST5_CFB64 */
 
 /* Define to 1 if you have the `EVP_des_cfb' function. */
-/* #undef HAVE_EVP_DES_CFB */
+/* #undef HAVE_EVP_DES_CFB64 */
 
 /* Define to 1 if you have the `EVP_des_ede3_cfb' function. */
-/* #undef HAVE_EVP_DES_EDE3_CFB */
+/* #undef HAVE_EVP_DES_EDE3_CFB64 */
 
 /* Define to 1 if you have the `EVP_idea_cfb' function. */
-/* #undef HAVE_EVP_IDEA_CFB */
+/* #undef HAVE_EVP_IDEA_CFB64 */
 
 /* Define to 1 if you have the `EVP_rc5_32_12_16_cfb' function. */
-/* #undef HAVE_EVP_RC5_32_12_16_CFB */
+/* #undef HAVE_EVP_RC5_32_12_16_CFB64 */
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 /* #undef HAVE_INTTYPES_H */
