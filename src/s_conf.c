58a7399a54a3ba654c83250a0a94ce8cdbae981f
Adam <Adam@anope.org>
2012-06-02 16:48:01 -0400

    Enable noctcp by default

--- src/s_conf.c
+++ src/s_conf.c
@@ -1915,7 +1915,7 @@ set_default_conf(void)
 	ConfigChannel.use_invex = YES;
 	ConfigChannel.use_except = YES;
 	ConfigChannel.use_knock = YES;
-	ConfigChannel.use_noctcp = NO;
+	ConfigChannel.use_noctcp = YES;
 	ConfigChannel.knock_delay = 300;
 	ConfigChannel.knock_delay_channel = 60;
 	ConfigChannel.max_chans_per_user = 15;
